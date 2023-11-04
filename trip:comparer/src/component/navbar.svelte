1f910d79821402fe6b974e7d902394bc6e439ddc
lanyimartin98 <lanyimartin@gmail.com>
2023-11-04 16:46:16 +0100

    Basic hamburger menu created for the below md screens.

--- trip:comparer/src/component/navbar.svelte
+++ trip:comparer/src/component/navbar.svelte
@@ -1,12 +1,40 @@
 <nav
-	class="flex flex-row justify-between m-2 p-2 shadow-md text-2xl backdrop-blur-md border-2 rounded-md"
+	class="flex flex-row justify-between m-2 p-2 shadow-md text-2xl backdrop-blur-md border-2 rounded-md {panelSize}"
 >
-	<h1 class="font-bold">trip:comparer</h1>
+<div>
+<button class="lg:hidden"><i class="bi bi-list" on:click={openPanel}></i></button>
+<div class="lg:flex lg:flex-row lg:items-center flex-col {panelOpen}">
+	<h1 class="font-bold p-1 lg:block hidden"><a href="/">trip:comparer</a></h1>
+	<a class="lg:hidden text-base p-1" href="/">About</a>
+	<a class="text-base p-1" href="/compare">Compare</a>
+	<a class=" text-base p-1" href="/licensing">Licensing</a>
+</div>
+</div>
+	
 	<div>
 		<a href="https://github.com/lanyimartin98" target="_blank"><i class="bi bi-github" /></a>
 		<a href="https://www.linkedin.com/in/martin-l%C3%A1nyi-0a5250233/" target="_blank"><i class="bi bi-linkedin" /></a>
 	</div>
 </nav>
+
+<script>
+	let open=false;
+
+	let panelOpen="hidden";
+	let panelSize="h-auto";
+
+	const openPanel=()=>{
+		open=!open;
+	}
+
+	$:if(open){
+		panelOpen="flex";
+		panelSize="h-30";
+	}else{
+		panelOpen="hidden";
+		panelSize="h-auto";
+	}
+</script>
 
 <style>
 	a:hover {
