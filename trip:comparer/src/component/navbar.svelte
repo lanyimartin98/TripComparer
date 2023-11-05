0c6284fc8c389064a0b92bff7ab6f5f96d825bbe
lanyimartin98 <lanyimartin@gmail.com>
2023-11-05 13:06:26 +0100

    Animation created.

--- trip:comparer/src/component/navbar.svelte
+++ trip:comparer/src/component/navbar.svelte
@@ -1,14 +1,17 @@
 <nav
-	class="flex flex-row justify-between m-2 p-2 shadow-md text-2xl backdrop-blur-md border-2 rounded-md {panelSize}"
+	class="flex flex-row justify-between m-2 p-2 shadow-md text-2xl backdrop-blur-md border-2 rounded-md"
 >
 <div>
-<button class="lg:hidden"><i class="bi bi-list" on:click={openPanel}></i></button>
-<div class="lg:flex lg:flex-row lg:items-center flex-col {panelOpen}">
+
+<button class="lg:hidden"><i class="bi {icon}" on:click={openClosePanel}></i></button>
+{#key animateHelper}
+<div class="lg:flex lg:flex-row lg:items-center flex-col {panelOpen}" transition:slide>
 	<h1 class="font-bold p-1 lg:block hidden"><a href="/">trip:comparer</a></h1>
-	<a class="lg:hidden text-base p-1" href="/">About</a>
-	<a class="text-base p-1" href="/compare">Compare</a>
-	<a class=" text-base p-1" href="/licensing">Licensing</a>
+	<a class="lg:hidden text-base p-1" href="/" on:click={openClosePanel}>About</a>
+	<a class="text-base p-1" href="/compare" on:click={openClosePanel}>Compare</a>
+	<a class=" text-base p-1" href="/licensing" on:click={openClosePanel}>Licensing</a>
 </div>
+{/key}
 </div>
 	
 	<div>
@@ -18,21 +21,25 @@
 </nav>
 
 <script>
-	let open=false;
+	import {slide, scale} from 'svelte/transition';
 
+	let isOpen=false;
 	let panelOpen="hidden";
-	let panelSize="h-auto";
+	let icon="bi-list";
 
-	const openPanel=()=>{
-		open=!open;
+	let animateHelper = {}
+
+	const openClosePanel=()=>{
+		isOpen=!isOpen;
+		animateHelper={};
 	}
 
-	$:if(open){
+	$:if(isOpen){
 		panelOpen="flex";
-		panelSize="h-30";
+		icon="bi-arrow-left";
 	}else{
 		panelOpen="hidden";
-		panelSize="h-auto";
+		icon="bi-list";
 	}
 </script>
 
